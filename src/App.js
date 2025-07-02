f5a20dbb2e5524d1105ea339a6fa520faf952af9
HarishTCX <kumardeepaktc@gmail.com>
2025-07-02 16:43:06 +0530

    CORS init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/hello`)
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error("API call failed:", error);
+      });
+  }, []);
+  console.log("API base URL:", process.env.REACT_APP_API_BASE_URL);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{message || "Loading..."}</h1>
     </div>
   );
 }
